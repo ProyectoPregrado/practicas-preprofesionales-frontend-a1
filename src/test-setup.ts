436cdf577e5bd864c707c7a9d51959fd9d410f09
Fariddy Traves <fariddy.traves@uta.edu.ec>
2026-09-22 15:50:20 -0500

    chore: configura compatibilidad de broadcastchannel en entorno de pruebas

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,2 +1,7 @@
 import '@testing-library/jest-dom/vitest'
 import 'fake-indexeddb/auto'
+
+if (typeof window !== 'undefined' && !window.BroadcastChannel && typeof globalThis.BroadcastChannel !== 'undefined') {
+  window.BroadcastChannel = globalThis.BroadcastChannel
+}
+
